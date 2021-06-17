4d7341da5c97fc983ff3e8b9a3c34b46083dd67b
Yony Calsin <helloyonycalsin@gmail.com>
2021-06-16 23:23:32 -0500

    feat: add the home header

--- lib/screens/home/index.dart
+++ lib/screens/home/index.dart
@@ -5,6 +5,40 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(color: const Color(0xFF2DBD3A));
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        actions: <Widget>[
+          IconButton(
+            onPressed: () => {},
+            icon: Icon(
+              Icons.info_outline,
+              color: Colors.blueAccent,
+            ),
+          )
+        ],
+      ),
+      body: Container(
+        padding: EdgeInsets.all(20),
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/images/home-cover.png"),
+            fit: BoxFit.contain,
+            alignment: Alignment.topLeft,
+          ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: 30,
+            ),
+            Text("This is my first application in flutter")
+          ],
+        ),
+      ),
+    );
   }
 }
